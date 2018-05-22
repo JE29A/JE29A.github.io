dd05e9768fb4b504302027577b54e717bf6c1412
J <jonathanefearn@gmail.com>
2018-05-22 11:03:13 -0700

    FTL enhancement

--- playbooks/js/controller_starships.js
+++ playbooks/js/controller_starships.js
@@ -1,6 +1,6 @@
 var app = angular.module('myApp',[]).controller("myPlaybooks", function($scope) {
   $scope.uniqueInputModel = {
-    FTL: {},
+    FTL: '',
     ships: {}
   };
 /***********************************************************************
@@ -59,6 +59,19 @@ var app = angular.module('myApp',[]).controller("myPlaybooks", function($scope) 
 });
 function getFTL() {
   var ret = [];
+
+  ret.push({
+    label: "None",
+    moves: [
+      [
+        {
+          class: "",
+          text: "Your ship lacks an FTL system. It does not have a FTL move."
+        }
+      ]
+    ]
+  });
+  
   ret.push({
     label: "Stardrive",
     moves: [
@@ -68,13 +81,13 @@ function getFTL() {
           text: "Starfall Move:"
         },{
           class: "",
-          text: "When "
+          text: " When "
         },{
           class: "font-bold",
-          text: "you plot a starfall under pressure"
+          text: "you plot a starfall under pressure or near a large gravity source"
         },{
           class: "",
-          text: ", roll+Smarts, +Cool, or +Maintenance (whichever is lowest). "
+          text: ", roll+Smarts or +Cool modified by +Maintenance. If you have ample time, still roll, but you treat a miss as a hit. "
         },{
           class: "font-bold",
           text: "On a hit"
@@ -100,7 +113,7 @@ function getFTL() {
           text: "Starrise Move:"
         },{
           class: "",
-          text: "After a ship has spent 121 hours in drivespace, roll+Maintenance. "
+          text: " After a ship has spent 121 hours in drivespace, roll+Maintenance. "
         },{
           class: "font-bold",
           text: "On a 10+"
@@ -123,22 +136,146 @@ function getFTL() {
       ]
     ]
   });
-  /*
+
   ret.push({
     label: "Hyperspace",
     moves: [
       [
         {
+          class: "font-bold",
+          text: "Gentle Operations: "
+        },{
           class: "",
-          text: "dfadfdd"
+          text: "When "
+        },{
+          class: "font-bold",
+          text: "you translate up or down under ideal circumstances"
+        },{
+          class: "",
+          text: ", you succeed with no damage or translation trauma."
+        }
+      ],[
+        {
+          class: "font-bold",
+          text: "Translate Up:"
+        },{
+          class: "",
+          text: " When "
+        },{
+          class: "font-bold",
+          text: "you translate up to a higher level of hyperspace under pressure"
+        },{
+          class: "",
+          text: ", roll+Smarts modified by +Maintenance. "
+        },{
+          class: "font-bold",
+          text: "On a hit"
+        },{
+          class: "",
+          text: ", you translate up successfully. "
+        },{
+          class: "font-bold",
+          text: "On a 10+"
+        },{
+          class: "",
+          text: ", you also retain much of your velocity."
+        },{
+          class: "font-bold",
+          text: "On a miss"
+        },{
+          class: "",
+          text: ", prepare for the worst!"
+        }
+      ],[
+        {
+          class: "font-bold",
+          text: "Crash Translation (down):"
+        },{
+          class: "",
+          text: " When "
+        },{
+          class: "font-bold",
+          text: "you initiate a multiple translation down or do it with a high velocity"
+        },{
+          class: "",
+          text: ", roll+Smarts modified by +Maintenance. Your ship successfully translates down, but your crash translation is very obvious to all gravity sensors with range on you. Each passengar has to make a "
+        },{
+          class: "font-bold",
+          text: "translation trauma"
+        },{
+          class: "",
+          text: "move. "
+        },{
+          class: "font-bold",
+          text: "On a hit"
+        },{
+          class: "",
+          text: ", your ship translated without suffering damage. "
+        },{
+          class: "font-bold",
+          text: "On a 10+"
+        },{
+          class: "",
+          text: ", you complete it as gently as possible: each passenger onboard has a +1 forward on their "
+        },{
+          class: "font-bold",
+          text: "translation trauma"
+        },{
+          class: "",
+          text: " move. "
+        },{
+          class: "font-bold",
+          text: "On a miss"
+        },{
+          class: "",
+          text: ", you still translate down, but also mark System Damage, Direct Hit, Hull Breach, or Adrift."
+        }
+      ],[
+        {
+          class: "font-bold",
+          text: "Translation Trauama:"
+        },{
+          class: "",
+          text: " When "
+        },{
+          class: "font-bold",
+          text: "you are onboard a ship during a crash translation"
+        },{
+          class: "",
+          text: ", roll+Tough. "
+        },{
+          class: "font-bold",
+          text: "On a 10+"
+        },{
+          class: "",
+          text: ", you suffer no meaningful symptoms. "
+        },{
+          class: "font-bold",
+          text: "On a 7-9"
+        },{
+          class: "",
+          text: ", you suffer from mild nausea and have a -1 ongoing for the next few minutes. "
+        },{
+          class: "font-bold",
+          text: "On a miss"
+        },{
+          class: "",
+          text: ", you are a mess and have a -1 ongoing for the next hour."
         }
       ]
     ]
   });
-  */
+
+  var i;
+  for(i = 0; i < ret.length; i++) {
+    ret[i].key = ret[i].label.replace(/ /g, '_').toLowerCase();
+  }
+
   ret.sort(function(a, b) {
-    if (a.label > b.label) { return 1; }
-    if (a.label < b.label) { return -1; }
+    if (a.key == 'none') { return -1; }
+    if (b.key == 'none') { return 1; }
+    if (a.key > b.key) { return 1; }
+    if (a.key < b.key) { return -1; }
     return 0;
   });
   return ret;
@@ -146,29 +283,22 @@ function getFTL() {
 
 function getShips() {
   var ret = [];
-  ret.push( genericShip() );
+  ret.push( generateShip({trope: 'Sheradin', size: 2, sizeLabel: 'Trader'} ) );
   
   return ret;
 }
 
-function autoShip(size, trope) {
-  if (!size || size < 1) {
-    size = 0;
-  }
-  if (!trope) {
-    trope = "Generic Ship "+ String(size);
-  }
+function generateShip(s) {
+  if (!s) { s = {}; }
   return {
-    trope: trope,
-    trope2: String(trope).replace(/ /g,"_"),
-    size: 1,
+    trope: s.trope || 'Generic',
+    trope2: (s.trope.replace(/ /g, '_') || 'Generic').toLowerCase(),
+    size: s.size || 1,
+    sizeLabel: s.sizeLabel || 'Ship',
     startingValues: {
-      payments: 10 + 10 * size,
+      payments: 10 + 5 * (s.size || 1),
       disposition: 0
     }
   };
 }
 
-function genericShip() {
-  return autoShip(1, 'Generic');
-}
